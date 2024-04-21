dec1cb4866a20738e83c8dc77bba2376e8ea8b8e
danuja01 <it21023378@my.sliit.lk>
2024-04-21 21:57:07 +0530

    Chore: Update ECS task definition

--- src/app.js
+++ src/app.js
@@ -55,8 +55,10 @@ connectDB();
 
 global.__basedir = __dirname;
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
+const MONGO_URI = process.env.MONGO_URI;
 
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
+  console.log(`Connected to MongoDB: ${MONGO_URI}`);
 });
